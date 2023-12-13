88ebcdfcf4019e0ba668bc73f2fcdb189d471aad
Cristina Román <cristinaroman05.cr@gmail.com>
2023-12-13 17:50:28 +0100

    avanzado

--- js/commons.js
+++ js/commons.js
@@ -8,3 +8,25 @@ const getRomanNumber = (number) => {
     while (i--) roman = (key[+digits.pop() + (i * 10)] || '') + roman;
     return Array(+digits.join('') + 1).join('M') + roman;
 }
+const mapOptions = (options, option) =>{
+    let optionFormated = [];
+
+    options.forEach((element, i) => {
+        const stringParts = element.split("/");
+        let idOption = stringParts[stringParts.length -2];
+        let auxObject ={
+            urlImg:'assets/images/' + option + '/' + idOption + 'jpg',
+            urlFetch: element
+        }
+        optionFormated.push(auxObject);
+    });
+    return optionFormated;
+}
+const addEventListenerToOptions = (option, options) => {
+    let optionLinks = [...document.getElementsByClassName(`detail__options-img--${option}`)];
+    optionLinks.forEach((element,i) => {
+        element.addEventListener('click', () => {
+            printPage(option.toUpperCase(), options[i].urlFetch);
+        });
+    });
+}
